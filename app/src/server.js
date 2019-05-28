18e131728063f044c7f27b6b4a926f5969a0f673
Ingo Kuba <ikuba@intension.de>
2019-05-28 09:17:26 +0200

    General improvements

--- app/src/server.js
+++ app/src/server.js
@@ -1,13 +1,18 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./boundary");
 
 // Constants
 const PORT = 8080;
-const HOST = "127.0.0.8";
+const HOST = "127.0.0.1";
 
 // App
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello world\n");
 });
